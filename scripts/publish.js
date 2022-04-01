535db70877a1176d28416ee5aec7ab45c040d3ef
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2022-04-01 22:45:07 +0200

    Add google search validation

--- scripts/publish.js
+++ scripts/publish.js
@@ -17,3 +17,9 @@ const sourcePathCNAME = upath.resolve(upath.dirname(__filename), '../src/CNAME')
 const destPathCNAME = upath.resolve(upath.dirname(__filename), '../docs');
     
 sh.cp('-R', sourcePathCNAME, destPathCNAME)
+
+// Copy over Google Search HTML (for verification)
+const sourcePathGOOGLE = upath.resolve(upath.dirname(__filename), '../src/google6e8a453e3ef3912b.html');
+const destPathGOOGLE = upath.resolve(upath.dirname(__filename), '../docs');
+    
+sh.cp('-R', sourcePathGOOGLE, destPathGOOGLE)
